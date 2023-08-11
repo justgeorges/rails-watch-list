05e6c60930fad1b2b61e5eb956de760d1acb46b8
George Arbanas <george.arbanas@icloud.com>
2023-08-11 16:28:48 +0900

    added delete functionality and basic styling

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,12 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
   def new
     @bookmark = Bookmark.new
-    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
     @bookmark.list = @list
     @bookmark.save
     if @bookmark.save
@@ -17,10 +17,16 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
